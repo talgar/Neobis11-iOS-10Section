a79575711dd4acecebc89f798cdeb902a56894e0
Talgar <talgarrmelisbekov@gmail.com>
2020-12-28 03:56:29 +0600

    fixed (added spinner) ready

--- Kinoafisha/Kinoafisha/Controllers/WebVC.swift
+++ Kinoafisha/Kinoafisha/Controllers/WebVC.swift
@@ -8,19 +8,45 @@
 import UIKit
 import WebKit
 
-class WebVC: UIViewController{
-
-    @IBOutlet weak var webView: WKWebView!
+class WebVC: UIViewController , WKNavigationDelegate, WKUIDelegate {
     
+    var webView: WKWebView!
+    var activityIndicator: UIActivityIndicatorView!
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        loadWebView()
+        webView = WKWebView(frame: CGRect.zero)
+        webView.navigationDelegate = self;
+        webView.uiDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+
+        activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.center = self.view.center
+        activityIndicator.style = UIActivityIndicatorView.Style.large
+        activityIndicator.color = .gray
+
+        webView.addSubview(activityIndicator)
+        view = webView
+
+        let load_url = URL(string: "https://kinoafisha.ua/")!
+        webView.load(URLRequest(url: load_url))
+        activityIndicator.startAnimating()
+
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        toolbarItems = [refresh]
+        navigationController?.isToolbarHidden = false
     }
 
-    func loadWebView(){
-        let url = URL(string: "https://kinoafisha.ua")!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+        activityIndicator.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activityIndicator.startAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        activityIndicator.stopAnimating()
     }
 }
-
